c2382b03b6659416124201f1a93fdf84ee23c0e0
Natalia <natalialllepost@gmail.com>
2021-04-22 15:45:44 +0200

    database

--- postgresql_django_site/settings.py
+++ postgresql_django_site/settings.py
@@ -82,22 +82,20 @@ WSGI_APPLICATION = 'postgresql_django_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DATABASE_NAME'),
-#         'USER': config('DATABASE_USER'),
-#         'PASSWORD': config('DATABASE_PASSWORD'),
-#         'HOST': config('DATABASE_URL'),
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.config(default='postgres://localhost')
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        # 'HOST': config('DATABASE_URL'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
